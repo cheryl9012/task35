405ca8d04e952b9b86a214bc28f70a433a430ccb
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:50:32 +0500

    Add files via upload

---
+++ Q35.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["Cat", "Lion", "Leopard", "Tiger"];
+animals.forEach(animals => {
+    console.log(` The ${animals}a member of the cat family and is a great predator.`);
+});
+console.log("These animals are great predators.");
---
+++ Q35.ts
@@ -0,0 +1,11 @@
+
+
+
+
+let animals: string[] = ["Cat", "Lion","Leopard","Tiger"]
+
+
+animals.forEach( animals => {
+    console.log(` The ${animals}a member of the cat family and is a great predator.`);
+});
+console.log("These animals are great predators.");
